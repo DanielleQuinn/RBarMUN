1a5cc37be20ab7e404e3492e3669a0b511382b2e
Danielle Quinn <danielle.quinn@acadiau.ca>
2017-06-05 04:18:27 -0230

    Updated CodeAlong script

--- IntroWorkshop/CodeAlong2.R
+++ IntroWorkshop/CodeAlong2.R
@@ -3,58 +3,65 @@
 # that R is going to communicate with when
 # importing files, exporting figures, etc.
 
-# Hint: To check your current working directory:
+# Prints your current working directory; no arguments needed
 getwd()
 
-# Hint: Use the tab key to easily navigate folders
-setwd("C:/Users/danie/Documents/GitHub/RBarMUN/15Nov2016")
+# You will need to change this to be your own working directory:
+setwd("C:/Users/danie/Desktop/RBarMUN") # Define your working directory
+  # Hint: Use the tab key to easily navigate folders
 
 # ---- Load Packages ----
-library(lubridate)
 library(dplyr)
+library(lubridate)
 
 # ---- Import Data ----
-# Single flat file saved as a .txt
 mydata<-read.delim("sampledata.txt")
+  # creates an object (container) called mydata which contains your 
+  # flat file (spreadsheet) sampledata.txt as a data frame with
+  # rows and columns
 
 # ---- Data Frames: Basics ----
 View(mydata) # Spreadsheet of data frame in new tab
-str(mydata) # Structure of each variable
-summary(mydata) # Summary of each variable
 dim(mydata) # Dimensions (rows, columns)
 head(mydata) # Output te first six rows
+str(mydata) # Structure of each variable
+summary(mydata) # Summary of each variable
 names(mydata) # Variable (column) names
 
 # ---- Data Frames: Each Column is a Vector ----
-mydata$length # Outputs the column length as a vector
+# Each column (variable) is denoted by $
+mydata$length # Outputs the contents of the column called length
+              # in the object mydata (which is a data frame) as a vector
+
+# Indexing works the same way as any other vector
 mydata$length[5] # Outputs the 5th element of the vector
 
-# Output a subset of the mydata$length vector
-# where length > 10
-mydata$length[mydata$length>10]
+# ---- Exercise 1: Conditional Indexing (Columns) ----
+# Create an object called adultlengths that contains all values
+# of the length column that are greater than 10
 
-# Note: To omit NAs, add a secondary argument
-mydata$length[mydata$length>10 & !is.na(mydata$length)]
+# What about NA values?
+# To omit NAs, add a secondary argument
 
 # ---- Data Frames: Data Frames are Two Dimensional ----
+# We need to provide two values (rows and columns) when indexing
+# R uses [row, column] assignment
 mydata[1,2] # Outputs the value of row 1, column 2
 
 mydata[1:5, c(1:3,6)] # Outputs the values of rows 1 to 5
-  # and columns 1, 2, 3, and 6
+                      # and columns 1, 2, 3, and 6
 
-# You can leave on value blank to select all
+# You can leave a dimension blank to select all
 mydata[,3] # Outputs all rows and column 3
 mydata[1:5,] # Outputs rows 1 to 5 and all columns
 
-# Output a subset of the mydata data frame
-# where length > 10
-mydata[mydata$length>10] # Why does this give you an error?
-
-# Correct:
-mydata[mydata$length>10,] # Need to include the 2nd dimension (column)
+# ---- Exercise 2: Conditional Indexing (Data Frames) ----
+# Create an object called adultdata that contains records
+# (i.e. all columns) of individuals with a length greater
+# than 10
 
 # ---- Data Frames: Modifying Variables ----
-# Add a new column (variable)
+# Add a new variable (column) called ratio
 mydata$ratio<-mydata$length/mydata$weight
 
 # Make the variable eggs represent presence/absence
@@ -64,18 +71,44 @@ mydata$eggs[mydata$eggs>0]<-1
 # Males should all have NA values for eggs
 mydata$eggs[mydata$sex=="m"]<-NA
 
-# ---- Exercise 1.6 ----
-# (A) Create a subset that contains males
-# captured in 2013
+# ---- Formatting Dates ----
+# Step 1: Create a character vector where each element
+# is formatted as "dd-mm-yyyy" from the year, month, and day columns
+# of mydata using the paste() function and place it in an object called myvec
+myvec<-paste(mydata$day, mydata$month, mydata$year, sep="-")
+  # sep argument defines what character we want to use to separate each component
+# Step 2: Use this object as the argument for dmy() and place the resulting
+# vector in an object called mydates
+mydates<-dmy(myvec)
+# Step 3: Create a new column called date which contains the vector
+# of formatted dates
+mydata$date<-mydates
 
-# (B) Find the average length of females
+# ALTERNATIVELY, nest all of these steps into a single line of code!
+mydata$dates<-dmy(paste(mydata$day, mydata$month, mydata$year, sep="-"))
+
+# ---- Exercise 3: Formatting Date Time ----
+# Use dmy_hms() to create a column in mydata called datetime
+# that contains both date and time information
 
 # ---- Exercise Solutions ----
-# Exercise 1.6
-# (A)
-male_2013<-mydata[mydata$sex=="m" & mydata$year==2013,]
+# Exercise 1
+# Create an object called adultlength that contains all values
+# of the length column that are greater than 10
+adultlengths<-mydata$length[mydata$length>10]
 
-# (B)
-female_meanlength<-mean(mydata$length[mydata$sex=="f" & !is.na(mydata$length)])
-# or
-female_meanlength<-mean(mydata$length[mydata$sex=="f"], na.rm=TRUE)
+# What about NA values?
+# To omit NAs, add a secondary argument
+adultlengths<-mydata$length[mydata$length>10 & !is.na(mydata$length)]
+
+# Exercise 2
+# Create an object called adultdata that contains records
+# (i.e. all columns) of individuals with a length greater
+# than 10
+adultdata<-mydata[mydata$length>10,] # Includes records with length is NA
+adultdata<-mydata[mydata$length>10 & !is.na(mydata$length),] # Omits records with length is NA
+
+# Exercise 3: Formatting Date Time
+# Use dmy_hms() to create a column in mydata called datetime
+# that contains both date and time information
+mydata$datetime<-dmy_hms(paste(mydata$day, mydata$month, mydata$year, mydata$hour, mydata$minute, mydata$second, sep="-"))
